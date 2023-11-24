6bf65703bbb3857cb2f2f53eefd33e08438263a1
VainerAriel <a@vainer.me>
2023-11-24 11:05:59 -0500

    Change color

--- Dinosaur-example/obstacle.py
+++ Dinosaur-example/obstacle.py
@@ -5,7 +5,7 @@ class Obstacle:
         self.x, self.y = x, groundLevel
         self.w, self.h = w, h
         self.rect = pg.Rect(self.x, self.y, self.w, self.h)
-        self.colour = (0, 0, 0)
+        self.colour = (50, 50, 200)
         self.pos = pg.math.Vector2(x, self.y - self.h)
         self.vel = pg.math.Vector2(-4, 0)
 
@@ -13,7 +13,7 @@ class Obstacle:
         self.x, self.y = x, y
         self.rect = pg.Rect(self.x, self.y, self.w, self.h)
         self.pos = pg.math.Vector2(x, y)
-    
+
     def move(self, rightBound):
         self.pos += self.vel
         self.rect = pg.Rect(self.pos.x, self.pos.y, self.w, self.h)
