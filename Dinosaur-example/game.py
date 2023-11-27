220a0c31aed6d1be9a4230e6fd7bde456ce62bd4
VainerAriel <a@vainer.me>
2023-11-27 10:52:31 -0500

    organize obstacle file, add Cacti class, add Bird class, make code more efficient and more readable. Add better obstacle spawning and make it compatible with as much different obstacles as needed.

--- Dinosaur-example/game.py
+++ Dinosaur-example/game.py
@@ -1,5 +1,7 @@
+import random
+
 import pygame as pg
-from obstacle import Obstacle
+from obstacle import Cacti, Bird
 from ground import Ground
 from random import randint, sample, choice
 from dinosaur import Dinosaur
@@ -12,8 +14,10 @@ class Game:
         self.W, self.H = screen.get_width(), screen.get_height()
         self.ground = Ground(self.H - 100)
         self.dino = Dinosaur(20, self.ground.y)
-        self.obstacleList = [Obstacle(self.W + 30, self.ground.y, 20, randint(40, 60))]
+
+        self.obstacleList = []
         self.maxObstacles = 15
+
         self.dinoScore = 0
         self.scoreTimer = 0
         self.clock = pg.time.Clock()
@@ -28,15 +32,33 @@ class Game:
 
     def updateObstacles(self):
         if len(self.obstacleList) < self.maxObstacles:
-            lastX = self.obstacleList[-1].pos.x
-            dist = self.W - lastX
-            offset = randint(dist, dist + 400) if dist > 0 else randint(100, 100 + 400)
-            self.obstacleList.append(Obstacle(lastX + offset, self.ground.y, 20, randint(40, 60)))
+            o_type = 0 if random.randint(0, 100) < 5 else 1
+            difficulty = 400
+            if len(self.obstacleList) > 0:
+                lastX = self.obstacleList[-1].pos.x + self.obstacleList[-1].size[0]
+                dist = self.W - lastX
+                offset = randint(dist, dist + difficulty) if dist > 0 else randint(100, 100 + difficulty)
+            else:
+                lastX = self.W
+                offset = randint(100, 100 + difficulty)
+
+            obstacle_size = 20, randint(40, 60)
+            if o_type == 1:
+                obstacle_size = 40, 20
+
+            obstacle_pos = lastX + offset, self.ground.y - obstacle_size[1]
+            if o_type == 1:
+                obstacle_pos = lastX + offset, self.ground.y - 20 - 25 * randint(0, 2)
+
+            if o_type == 0:
+                self.obstacleList.append(Cacti(obstacle_pos, obstacle_size, (-4, 0)))
+            elif o_type == 1:
+                self.obstacleList.append(Bird(obstacle_pos, obstacle_size, (-4, 0)))
 
         for o in self.obstacleList[::-1]:
-            o.move(self.W)
-            if o.pos.x < -20:
-                self.obstacleList.remove(self.obstacleList[0])
+            o.move()
+            if o.pos.x < -o.size[0]:
+                self.obstacleList.remove(o)
             
             if self.dino.collide(o):
                 self.dino.kill()
