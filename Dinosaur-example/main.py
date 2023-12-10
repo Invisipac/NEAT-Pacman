5f071757ecc6490ca98f0fe185ee824fd543b372
VainerAriel <a@vainer.me>
2023-12-10 13:49:37 -0500

    Add implementation of NEAT for dino game, add stats and speed control.

--- Dinosaur-example/main.py
+++ Dinosaur-example/main.py
@@ -1,8 +1,19 @@
 import pygame as pg
+import os
+
 from game import Game
+
 W, H = 800, 600
 
 screen = pg.display.set_mode((W, H))
 game = Game(screen)
 
-game.main()
+# game.main()
+
+
+
+
+if __name__ == '__main__':
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, "config.txt")
+    game.run(config_path)
