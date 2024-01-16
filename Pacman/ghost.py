3ba19851eaceba06baaf2d5b26b31e4ccb96c9b3
Invisipac <invisiblepacman@gmail.com>
2024-01-15 23:33:16 -0500

    Make ghost run in opposite direction

--- Pacman/ghost.py
+++ Pacman/ghost.py
@@ -3,6 +3,7 @@ from pygame.math import Vector2 as vec
 from astar import astar
 from pacman import Pacman
 import random
+import math
 class Ghost:
     def __init__(self, pos, speed, colour, pacman: Pacman) -> None:
         self.pos = vec(pos[0]*RATIO[0], pos[1]*RATIO[1])
@@ -14,8 +15,15 @@ class Ghost:
         self.start = (int(self.map_locs.y), int(self.map_locs.x))
         self.cur_path = list(reversed(astar(self.start, self.start, map)))
         self.pacman = pacman
-        self.state = "Scatter"
-        self.random_target = self.find_random_pos()
+        self.state = "Chase"
+        self.target = (0, 0)
+        self.start_scared = True
+        self.timer = 0
+        # self.random_target = self.find_random_pos()
+        # self.need_new_scared_path = True
+        # self.scared_positions = []
+        # self.scare_radius = 8
+        #self.scared_pos = self.find_scared_pos()
         # self.random_pos = (0, 0)
 
 
@@ -27,18 +35,106 @@ class Ghost:
             self.map_locs.y = self.pos.y // RATIO[1]
             self.pos.y = self.map_locs.y * RATIO[1] + RATIO[1] / 2
     
-    def update(self, target):
+    def update(self):
+        self.timer += 0.1
         if self.state == "Chase":
-            target = (int(self.pacman.map_locs.y), int(self.pacman.map_locs.x))
-        elif self.state == "Scatter":
-            target = self.random_target
-            if len(self.cur_path) < 4:
-                target = self.find_random_pos()
-                self.random_target = target
-        self.find_map_loc()
-        self.start = (int(self.map_locs.y), int(self.map_locs.x))        
-        self.cur_path = list(reversed(astar(self.start, target, map)))
+            self.target = (int(self.pacman.map_locs.y), int(self.pacman.map_locs.x))
+            self.find_map_loc()
+            self.start = (int(self.map_locs.y), int(self.map_locs.x))        
+            self.cur_path = list(reversed(astar(self.start, self.target, map)))
+            self.calculate_dir()
+            if self.timer > 10:
+                self.state = "Scared"
+                self.start_scared = True
+                self.timer = 0
+        elif self.state == "Scared":
+            self.move_scared()
+            if self.timer > 10:
+                self.state = "Chase"
+                self.timer = 0
+        # elif self.state == "Scatter":
+        #     #target = self.random_target
+        #     if len(self.cur_path) == 1:
+        #         self.target = self.find_random_pos()
+        #         #self.random_target = target
+        # elif self.state == "Scared":
+        #     self.find_scared_pos()
+        
+    
+    def dist_to_pacman(self):
+        return math.sqrt((self.pos.x - self.pacman.pos.x)**2 + (self.pos.y - self.pacman.pos.y)**2)
+    
+    # def find_scared_pos(self):
+    #     if self.need_new_scared_path:
+    #         self.scared_positions = []
+    #         for i, l in enumerate(map):
+    #             for j, c in enumerate(l):
+    #                 if ((c in NODES or c in POWER_DOTS)
+    #                     and (i < self.pacman.map_locs.y - self.scare_radius or i > self.pacman.map_locs.y + self.scare_radius)
+    #                     and (j < self.pacman.map_locs.x - self.scare_radius or j > self.pacman.map_locs.x + self.scare_radius)):
+    #                     self.scared_positions.append((i, j))
+    #         print(self.scared_positions)
+    #         self.target = random.choice(self.scared_positions)
+    #         self.need_new_scared_path = False
+    #     elif len(self.cur_path) == 1:#self.dist_to_pacman() <= self.scare_radius * 24:
+    #         self.need_new_scared_path = True
+       
+    def move_scared(self):
+        l, r = 0, 0
+        if self.start_scared:
+            
+            self.dir = [0, 0]
+            x_diff = self.pacman.pos.x - self.pos.x
+            y_diff = self.pacman.pos.y - self.pos.y
 
+            if self.pacman.dir == "RIGHT" or self.pacman.dir == "LEFT":
+                if x_diff != 0:
+                    self.dir[0] = -int(x_diff/abs(x_diff))
+                else:
+                    self.dir[0] = 0
+                    self.dir[1] = -int(y_diff/abs(y_diff))
+            else:
+                if y_diff != 0:
+                    self.dir[1] = -int(y_diff/abs(y_diff))
+                else:
+                    self.dir[1] = 0
+                    self.dir[0] = -int(x_diff/abs(x_diff))
+            print(self.pacman.dir, x_diff, y_diff, self.dir)
+            # if y_diff != 0:
+            #     self.dir[1] = y_diff//y_diff
+            # else:
+            #     self.dir[1] = 0
+
+            # if self.pacman.dir == "UP":
+            #     self.dir = [0, -1]
+            # if self.pacman.dir == "DOWN":
+            #     self.dir = [0, 1]
+            # if self.pacman.dir == "LEFT":
+            #     self.dir = [-1, 0]
+            # if self.pacman.dir == "RIGHT":
+            #     self.dir = [1, 0]
+            self.start_scared = False
+
+        
+        row = int(self.map_locs.y) 
+        col = int(self.map_locs.x)
+        self.find_map_loc()
+        #some bug that dir is not an int but a float but the value is right???
+        #temp fix
+        if map[row + self.dir[1]][col + self.dir[0]] in OBSTACLES:
+            self.dir[0], self.dir[1] = self.dir[1], self.dir[0]
+            if map[row + self.dir[1]][col + self.dir[0]] in OBSTACLES:
+                r = 1
+                self.dir[0] *= -1
+                self.dir[1] *= -1
+            elif map[row - self.dir[1]][col - self.dir[0]] in OBSTACLES:
+                l = 1
+            
+            if l + r == 0:
+                c = random.choice([-1, 1])
+                self.dir[0] *= c
+                self.dir[1] *= c
+            
     def calculate_dir(self):
         # Astar_path = list(reversed(astar(start, goal, grid)))
         # print(Astar_path)
@@ -48,11 +144,11 @@ class Ghost:
             y_diff = self.cur_path[1][0] - self.start[0]
 
             if x_diff != 0:
-                self.dir[0] = (x_diff)/abs(x_diff)
+                self.dir[0] = (x_diff)//abs(x_diff)
             else:
                 self.dir[0] = 0
             if y_diff != 0:
-                self.dir[1] = (y_diff)/abs(y_diff)
+                self.dir[1] = (y_diff)//abs(y_diff)
             else:
                 self.dir[1] = 0
         
@@ -77,8 +173,8 @@ class Ghost:
 
         row = int(self.map_locs.y + self.dir[1])
         col = int(self.map_locs.x + self.dir[0])
-
-        self.calculate_dir()
+        
+        
 
         if map[row][col] in PATH and self.pos.x >= 0 and self.pos.x <= WIDTH:
             self.pos += vec(self.dir[0]*RATIO[0]/self.speed[0], self.dir[1]*RATIO[1]/self.speed[1])
