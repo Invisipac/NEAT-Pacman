aed540012d8629659485e1d00107a59393de861c
Invisipac <invisiblepacman@gmail.com>
2024-01-16 18:32:39 -0500

    ALmost fix scared movement bug

--- Pacman/ghost.py
+++ Pacman/ghost.py
@@ -7,42 +7,35 @@ import math
 class Ghost:
     def __init__(self, pos, speed, colour, pacman: Pacman) -> None:
         self.pos = vec(pos[0]*RATIO[0], pos[1]*RATIO[1])
-        self.map_locs = vec(*pos)
+        self.map_locs = vec(int(pos[0]), int(pos[1]))
         self.speed = vec(speed, speed)
-        self.dir = [0, 0]
+        self.dir = [-1, 0]
         self.colour = colour
         self.r = 10
         self.start = (int(self.map_locs.y), int(self.map_locs.x))
         self.cur_path = list(reversed(astar(self.start, self.start, map)))
         self.pacman = pacman
         self.state = "Chase"
-        self.target = (0, 0)
+        self.target = (-10, -10)
         self.start_scared = True
         self.timer = 0
         self.can_move = True
-        # self.random_target = self.find_random_pos()
-        # self.need_new_scared_path = True
-        # self.scared_positions = []
-        # self.scare_radius = 8
-        #self.scared_pos = self.find_scared_pos()
-        # self.random_pos = (0, 0)
+        self.once = True
 
 
     def find_map_loc(self):
         if (self.pos.x - RATIO[0] / 2) % RATIO[0] <= 1:
             self.map_locs.x = self.pos.x // RATIO[0]
             self.pos.x = self.map_locs.x* RATIO[0] + RATIO[0] / 2 
-            self.can_move = True
         if (self.pos.y - RATIO[1] / 2) % RATIO[1] <= 1:
             self.map_locs.y = self.pos.y // RATIO[1]
             self.pos.y = self.map_locs.y * RATIO[1] + RATIO[1] / 2
-            self.can_move = True
     
     def update(self):
         self.timer += 0.1
         if self.state == "Chase":
             self.target = (int(self.pacman.map_locs.y), int(self.pacman.map_locs.x))
-            self.find_map_loc()
+            # self.find_map_loc()
             self.start = (int(self.map_locs.y), int(self.map_locs.x))        
             self.cur_path = list(reversed(astar(self.start, self.target, map)))
             self.calculate_dir()
@@ -55,13 +48,6 @@ class Ghost:
             if self.timer > 10:
                 self.state = "Chase"
                 self.timer = 0
-        # elif self.state == "Scatter":
-        #     #target = self.random_target
-        #     if len(self.cur_path) == 1:
-        #         self.target = self.find_random_pos()
-        #         #self.random_target = target
-        # elif self.state == "Scared":
-        #     self.find_scared_pos()
         
     
     def dist_to_pacman(self):
@@ -101,28 +87,32 @@ class Ghost:
                 else:
                     self.dir[1] = 0
                     self.dir[0] = -int(x_diff/abs(x_diff))
-            print(self.pacman.dir, x_diff, y_diff, self.dir)
+            # print(self.pacman.dir, x_diff, y_diff, self.dir)
             self.start_scared = False
 
-        
-        row = int(self.map_locs.y) 
-        col = int(self.map_locs.x)
-        # self.find_map_loc()
-        #some bug that dir is not an int but a float but the value is right???
-        #temp fix
-
-        if self.can_move:
-            possible_nodes = []
-            possible_nodes.append((col + self.dir[0], row + self.dir[1]))
-            possible_nodes.append((col + self.dir[1], row + self.dir[0]))
-            possible_nodes.append((col - self.dir[1], row - self.dir[0]))
+        if self.once or self.target == self.map_locs:
+            row = int(self.map_locs.y) 
+            col = int(self.map_locs.x)
+            # self.find_map_loc()
+            #some bug that dir is not an int but a float but the value is right???
+            #temp fix
+            #print(map[23][15])
+            # if self.can_move:
+            possible_nodes, not_possible_nodes = [], []
+            possible_nodes.append((int(col + 1), int(row)))
+            possible_nodes.append((int(col - 1), int(row)))
+            possible_nodes.append((int(col), int(row + 1)))
+            possible_nodes.append((int(col), int(row - 1)))
             for n in possible_nodes:
-                if map[int(n[1])][int(n[0])] in OBSTACLES:
-                    possible_nodes.remove(n)
+                if map[n[1]][n[0]] == 'w' or n == (int(col - self.dir[0]), int(row - self.dir[1])):
+                    not_possible_nodes.append(n)
+            for n in not_possible_nodes:
+                possible_nodes.remove(n)
             self.target = random.choice(possible_nodes)
-            print(self.target, self.map_locs, possible_nodes)
+            print(map[self.target[1]][self.target[0]], self.dir, self.target, self.map_locs, possible_nodes)
             self.move_towards(self.target)
-            self.can_move = False
+            self.once = False
+            # self.can_move = False
 
             
     def calculate_dir(self):
