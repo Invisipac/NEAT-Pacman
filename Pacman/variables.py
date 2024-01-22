6f5153ad6e644c1081ff3bfa3bbad500706c5cd3
VainerAriel <a@vainer.me>
2024-01-22 01:14:36 -0500

    quick fix to importing files

--- Pacman/variables.py
+++ Pacman/variables.py
@@ -9,7 +9,10 @@ WIDTH, HEIGHT = RATIO[0] * GRID_SIZE[0], RATIO[1] * GRID_SIZE[1]
 
 display = pg.display.set_mode((WIDTH, HEIGHT))
 
-map = open("./Pacman/true-map.txt", "r").read().split()
+# fix_files = "./Pacman/"
+fix_files = ""
+
+map = open(f"{fix_files}true-map.txt", "r").read().split()
 
 EXIT = "E"
 NODES = ["n", "N", "&"]
@@ -29,7 +32,7 @@ def loadify(filename):
     return pygame.image.load(filename).convert_alpha()
 
 
-bg = loadify("./Pacman/Sprites//Background//bg.png")
+bg = loadify(f"{fix_files}Sprites//Background//bg.png")
 
 ghost_sprites = []
 ghost_names = ["Blinky", "Pinky", "Inky", "Clyde"]
@@ -38,26 +41,26 @@ for i in range(4):
     for j in range(8):
         if j % 2 == 0:
             ghost_sprites[i].append([])
-        ghost_sprites[i][j // 2].append(loadify(f"./Pacman/Sprites//Ghosts//{ghost_names[i]}//ghost{j}.png"))
+        ghost_sprites[i][j // 2].append(loadify(f"{fix_files}Sprites//Ghosts//{ghost_names[i]}//ghost{j}.png"))
 
 dead_ghost_sprites = []
 for i in range(4):
     if i % 2 == 0:
         dead_ghost_sprites.append([])
-    dead_ghost_sprites[i // 2].append(loadify(f"./Pacman/Sprites//Ghosts//Dead//deadghost{i}.png"))
+    dead_ghost_sprites[i // 2].append(loadify(f"{fix_files}Sprites//Ghosts//Dead//deadghost{i}.png"))
 
 dead_ghost_sprites.append([])
 for i in range(4):
-    dead_ghost_sprites[2].append(loadify(f"./Pacman/Sprites//Ghosts//Eyes//eyes{i}.png"))
+    dead_ghost_sprites[2].append(loadify(f"{fix_files}Sprites//Ghosts//Eyes//eyes{i}.png"))
 
 pacman_sprites = []
 pacman_sprites.append([])
 for i in range(9):
     if i % 2 == 0:
         pacman_sprites[0].append([])
-    pacman_sprites[0][i // 2].append(loadify(f"./Pacman/Sprites//Pacman//pacman{i}.png"))
+    pacman_sprites[0][i // 2].append(loadify(f"{fix_files}Sprites//Pacman//pacman{i}.png"))
 pacman_sprites.append([])
 for i in range(14):
-    pacman_sprites[1].append(loadify(f"./Pacman/Sprites//Pacman//Dying-animation//pacman{i}.png"))
+    pacman_sprites[1].append(loadify(f"{fix_files}Sprites//Pacman//Dying-animation//pacman{i}.png"))
 
 print(pacman_sprites)
