d6646e281951d09da6f31c13e77be55974ac94d6
Invisipac <invisiblepacman@gmail.com>
2024-01-24 11:05:54 -0500

    Add a small comment

--- Dinosaur-example/main.py
+++ Dinosaur-example/main.py
@@ -14,4 +14,5 @@ if __name__ == '__main__':
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, "config.txt")
     game.run(config_path)
+    # -------- run replay after at least one run of the game.run function ------------
     # game.replayGenome(config_path)
